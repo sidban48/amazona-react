3d32b6fd2ec332b1d1039faa3aae1508774136bc
sidban48 <siddharthbansal48@gmail.com>
2022-04-30 22:47:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+      <a href = "/">Home</a>
+      </header>
+      <main>Product List</main>
+      <footer>
+        Made by Siddharth Bansal(only for educational purposes)
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
